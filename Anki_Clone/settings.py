4c0e9f4cf59f1241d92ed771f42fda7d1070f049
Arman Tarkhanian <atarkhanian@tynker.com>
2023-05-12 14:51:37 -0700

    updated external hostname

--- Anki_Clone/settings.py
+++ Anki_Clone/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-@6mh@$+0#vw0!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 # Add your application name here
